e2ec0576b3380e0cd0e1ea5c0fd37e64d2dd6cfa
prodbysky <kyrbynyt@gmail.com>
2024-06-10 07:18:49 +0300

    New tokenizing schema?

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::read_to_string;
 use std::str::FromStr;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Token {
     Mut,
     TypeSpecifier(String),
@@ -9,6 +9,12 @@ enum Token {
     Equals,
     Semicolon,
     Symbol(String),
+    LParen,
+    RParen,
+    Minus,
+    Plus,
+    Star,
+    FSlash,
 }
 
 #[derive(Debug)]
@@ -24,6 +30,12 @@ impl FromStr for Token {
             "=" => Ok(Self::Equals),
             ";" => Ok(Self::Semicolon),
             "i32" => Ok(Self::TypeSpecifier(s)),
+            "(" => Ok(Self::LParen),
+            ")" => Ok(Self::RParen),
+            "+" => Ok(Self::Plus),
+            "-" => Ok(Self::Minus),
+            "*" => Ok(Self::Star),
+            "/" => Ok(Self::FSlash),
             _ => {
                 if let Ok(_) = s.parse::<i32>() {
                     Ok(Self::NumberLiteral(s))
@@ -35,45 +47,132 @@ impl FromStr for Token {
     }
 }
 
-fn split_words(source: &str) -> Vec<String> {
-    let mut words = vec![];
-    let mut iter = source.chars().peekable();
-    while let Some(mut c) = iter.next() {
-        if c.is_whitespace() {
-            continue;
+struct Tokenizer {
+    source: String,
+    pos: usize,
+    current_char: Option<char>,
+}
+
+impl Tokenizer {
+    fn new(source: String) -> Self {
+        let mut tokenizer = Self {
+            source,
+            pos: 0,
+            current_char: None,
+        };
+        if !tokenizer.source.is_empty() {
+            tokenizer.current_char = Some(tokenizer.source.as_bytes()[0] as char);
         }
 
-        let mut buf = String::new();
-        if c == ';' {
-            words.push(";".to_string());
-            continue;
-        }
+        return tokenizer;
+    }
 
-        buf.push(c);
-        c = iter.next().unwrap();
+    fn advance(&mut self) {
+        self.pos += 1;
 
-        while !c.is_whitespace() {
-            if c == ';' {
-                words.push(buf.clone());
-                words.push(c.to_string());
-                c = iter.next().unwrap();
-                buf.clear();
-                continue;
-            }
-            buf.push(c);
-            c = iter.next().unwrap();
-        }
-
-        if !buf.is_empty() {
-            words.push(buf);
+        if self.pos > self.source.len() - 1 {
+            self.current_char = None; // EOF
+        } else {
+            self.current_char = Some(self.source.as_bytes()[self.pos] as char);
         }
     }
 
-    words
+    fn skip_whitespace(&mut self) {
+        while let Some(c) = self.current_char {
+            if c.is_whitespace() {
+                self.advance();
+            } else {
+                return;
+            }
+        }
+    }
+
+    fn number_literal(&mut self) -> String {
+        let mut buf = String::new();
+        while let Some(c) = self.current_char {
+            if c.is_digit(10) {
+                buf.push(c);
+                self.advance();
+            } else {
+                return buf;
+            }
+        }
+        buf
+    }
+
+    fn symbol(&mut self) -> String {
+        match self.current_char {
+            Some('+') => {
+                self.advance();
+                "+".to_string()
+            }
+            Some('-') => {
+                self.advance();
+                "-".to_string()
+            }
+            Some('=') => {
+                self.advance();
+                '='.to_string()
+            }
+            Some(';') => {
+                self.advance();
+                ';'.to_string()
+            }
+            Some('(') => {
+                self.advance();
+                '('.to_string()
+            }
+            Some(')') => {
+                self.advance();
+                ')'.to_string()
+            }
+            Some('*') => {
+                self.advance();
+                '*'.to_string()
+            }
+            Some('/') => {
+                self.advance();
+                '/'.to_string()
+            }
+            _ => {
+                let mut buffer = String::new();
+                while let Some(c) = self.current_char {
+                    if c.is_whitespace() || c == ';' {
+                        break;
+                    }
+                    buffer.push(c);
+                    self.advance();
+                }
+                buffer
+            }
+        }
+    }
 }
 
-fn tokenize(words: Vec<String>) -> Vec<Token> {
-    return words.iter().map(|w| Token::from_str(w).unwrap()).collect();
+impl Iterator for Tokenizer {
+    type Item = Token;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(c) = self.current_char {
+            if c.is_whitespace() {
+                self.skip_whitespace();
+                continue;
+            }
+
+            if c.is_digit(10) {
+                return Some(Token::NumberLiteral(self.number_literal()));
+            }
+
+            let symbol = self.symbol();
+            if let Ok(token) = Token::from_str(&symbol) {
+                return Some(token);
+            }
+
+            return None;
+        }
+
+        None
+    }
 }
 
 fn main() {
@@ -85,8 +184,9 @@ fn main() {
     }
 
     let src = read_to_string(&args[1]).expect("Couldn't read provided source file");
+    let tokenizer = Tokenizer::new(src);
 
-    let tokens = tokenize(split_words(&src));
-
-    println!("{:?}", tokens);
+    for token in tokenizer {
+        println!("{:?}", token);
+    }
 }
